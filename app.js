51dfb388abdb8ed416ec7056764cfc1bf2665cb4
juanchimerlo10 <67029243+juanchimerlo10@users.noreply.github.com>
2020-10-17 12:42:25 -0300

    registro a validar

--- app.js
+++ app.js
@@ -1,23 +1,55 @@
-const nombre =document.getElementById("nombre")
-const email = document.getElementById("email")
-const form = document.getElementById("form")
-// VALIDACION DE EMAIL
-// const validateEmail = () => {
-//     let email = document.getElementById("email");
+let registrados = []
+class Persona { 
+constructor(nombre, apellido,email, celular, factor){
+    this.nombre = nombre
+    this.apellido = apellido
+    this.email = email
+    this.celular = celular
+    this.factor = factor
+}
+}
 
-//     if (email.value.match(/([a-z]\w+@[a-z]+.[a-z]{2,5})/)){
-//         console.log("Correo correcto");
-//     } else {
-//         console.error("No cumple el formato de correo")
+
+let nombre =document.getElementById("nombre")
+let email = document.getElementById("email")
+let form = document.getElementById("form")
+let apellido = document.getElementById('apellido')
+let celular = document.getElementById('celular')
+let factor = document.getElementById('factorSanguineo')
+let warnings = document.getElementById('warnings')
+
+function arreglo (event){
+    event.preventDefault()
+    let persona = new Persona ( 
+        nombre.value,
+        apellido.value,
+         email.value,
+         celular.value,
+         factor.value)
+         console.log(persona)
+    registrados.push(persona)
+    nombre.value = ''
+    apellido.value=''
+    email.value = ''
+    celular.value = ''
+    factor.value = ''
+
+    localStorage.setItem('registrados', JSON.stringify (registrados))
+}
+
+
+
+// Validaciones 
+// form.addEventListener('submit', e => {
+//     let warnings = ''
+//     let entrar = false
+//     let valEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+//     if (nombre.value.length < 4){
+//         warnings += `Nombre invalido <br>`
+//         entrar = true
+//     } if (!valEmail.test(email.value)){
+//         warnings += `Email invalido <br>`
+//         entrar = true
 //     }
-// }
-
-form.addEventListener("submit", e => {
-    e.preventDefault(),
-
-    let validarEmail = /[a-z]\w+@[a-z]+.[a-z]{2,5})/;
-    if (!validarEmai.test(email.value)){
-        
-    }
-
-})
+    
+// })
