bf4b0142386e5526ffc57f8662c70b723e125718
juanchimerlo10 <juanchimerlo10@gmail.com>
2020-10-26 19:25:46 -0300

    s
    s

--- js/admin.js
+++ js/admin.js
@@ -1,84 +1,26 @@
 const admin = {
-    nombre: "admin"
-    password: "admin",
+    user: "admin",
+    pass: "admin",
 }
 
-localStorage.setItem("inicioSesion", JSON.stringify(usuario))
-localStorage.setItem("auth", "false")
 
+localStorage.setItem("login", JSON.stringify(admin))
 
-let btnLogin = document.getElementById("inicioSesion")
-
-let login = localStorage.getItem("auth")
-
-
-//validacion de login => pagina admin
-const validation = (login) => {
-    let paginaAdmin = location.pathname
-    if (paginaAdmin === "/admin.hmtl") {
-        if (login === "false") {
-            // $("#modalLogin").modal("show")
-            location = "principal.html"
-        }
-
-    }
-}
-
-const loguear = () => {
-    if (login === "true") {
-        btnLogin.innerHTML = btnCerrar;
-    } else {
-        btnLogin.innerHTML = btnInicio
-    }
-}
-const logueoChange = () => {
-    let usuario = document.getElementById("user").value
-    let password = document.getElementById("password").value
-
-    let dato = JSON.parse(localStorage.getItem("login"))
-    if (usuario === "" || password === "") {
-        return alert("Debe ingresar valores en los campos")
-    }
-    if (dato.nombre === usuario) {
-        if (dato.password === password) {
-            login = true
-            loguear()
-            localStorage.setItem("auth", "true")
-            window.location = "admin.html"
+() {
+    let name = document.getElementById("name")
+    console.log("hola")
+    let password = document.getElementById("password")
+    if ("login" in localStorage) {
+        let usuario = JSON.parse(localStorage.getItem("login"))
+        if (usuario.user == name.value && usuario.pass == password.value) {
+            console.log(usuario);
+            location.replace("/admin.html")
+            return
         } else {
-            alert("Usuario o contraseña incorrectos")
+            alert("Usuario o contraseña incorrecto")
+            return
         }
-    } else {
-        alert("Usuario o contraseña incorrectos")
     }
-    document.getElementById("user").value = ""
-    document.getElementById("password").value = ""
-
-    const desloguear = () => {
-        login = false
-        alert("Has sido deslogueado")
-        localStorage.setItem("auth", "false")
-        loguear()
-        window.location = "pricipal.html"
-    }
+}
 
 
-    let btnInicio = `<button 
-            id="botonLogin"
-            class="btn btn-outline-secondary"
-            type="button"
-            data-toggle="modal"
-            data-target="staticBackdrop"
-            >
-            Iniciar Sesion
-            </button>`;
-    let btnCerrar = `<button 
-            id="botonSalir"
-            class="btn btn-outline-secondary"
-            type="button"
-            onclick="desloguear()"
-            >
-            Cerrar Sesion
-            </button>`;
-    loguear();
-    validation(login)
